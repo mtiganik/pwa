215be493d4fe70969863331b1c49ecc22970aeff
Mihkel Tiganik <mtiganik@gmail.com>
2023-12-31 16:48:28 +0200

    start of service implementation

--- src/service/category-service.tsx
+++ src/service/category-service.tsx
@@ -1,7 +1,20 @@
 import Category from "../models/category";
 import axios from "axios";
+import { getAuthorizationHeader,handle401Error } from "./utils";
+import GetUrl from "../utils/get-url";
 
+const AuthHeader = getAuthorizationHeader()
+const url = GetUrl() + "TodoCategories"
+const contentType = {'Content-Type': 'application/json'}
 export const getAllCategories = async(): Promise<Category[]> => {
+  try{
+    var response = await axios.get(url, {headers:{...AuthHeader, ...contentType}})
+    if(response.status === 401) {handle401Error()}
+    return response.data
+  }catch(error){
+    console.error("Error getting categories:", error)
+    return []
+  }
   return []
 }
 export const getCategoryById = async(catId: string): Promise<Category> => {
@@ -9,6 +22,13 @@ export const getCategoryById = async(catId: string): Promise<Category> => {
 }
 
 export const postCategory = async( cat: Category): Promise<number> => {
+  try{
+    const AuthHeader = getAuthorizationHeader()
+    var response = await axios.post(url,
+      )
+  }catch(error){
+
+  }
   return 0
 }
 
